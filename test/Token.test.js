823faf90d7ad265e2846306d84c284cd7aaa35f8
Anar Enhsaihan <aenhsaihan@gmail.com>
2020-06-18 23:07:05 -0400

    Remove console logs detailing transfer balances
    Console logs were in place to show the balance of sender and recipient
    before and after transfer. Tests have been put in place to show these
    balances now.

--- test/Token.test.js
+++ test/Token.test.js
@@ -47,10 +47,6 @@ contract("Token", ([deployer, receiver]) => {
   describe("sending tokens", () => {
     it("transfers token balances", async () => {
       let balanceOf;
-      balanceOf = await token.balanceOf(deployer);
-      console.log("deployer balanceb before transfer", balanceOf.toString());
-      balanceOf = await token.balanceOf(receiver);
-      console.log("receiver balance before transfer", balanceOf.toString());
 
       await token.transfer(receiver, tokens(100), {
         from: deployer,
@@ -58,10 +54,9 @@ contract("Token", ([deployer, receiver]) => {
 
       balanceOf = await token.balanceOf(deployer);
       balanceOf.toString().should.equal(tokens(999900).toString());
-      console.log("deployer balanceb after transfer", balanceOf.toString());
+
       balanceOf = await token.balanceOf(receiver);
       balanceOf.toString().should.equal(tokens(100).toString());
-      console.log("receiver balance after transfer", balanceOf.toString());
     });
   });
 });
