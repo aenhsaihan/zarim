1581a4d03227549f406219a12fc36d2cea3a2936
Anar Enhsaihan <aenhsaihan@gmail.com>
2020-06-22 19:43:57 -0400

    Refactor transfer function as internal function
    Make transfer function internal so transfer logic can be shared between
    public transfer and transferFrom functions.

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -30,15 +30,23 @@ contract Token {
         returns (bool success)
     {
         require(_to != address(0), "Recipient address is invalid");
+        _transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function _transfer(
+        address _from,
+        address _to,
+        uint256 _value
+    ) internal returns (bool success) {
         require(
             balanceOf[msg.sender] >= _value,
             "Sender has insufficient funds"
         );
 
-        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+        balanceOf[_from] = balanceOf[_from].sub(_value);
         balanceOf[_to] = balanceOf[_to].add(_value);
-        emit Transfer(msg.sender, _to, _value);
-        return true;
+        emit Transfer(_from, _to, _value);
     }
 
     function approve(address _spender, uint256 _value)
@@ -48,6 +56,14 @@ contract Token {
         require(_spender != address(0), "Recipient address is invalid");
         allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
         return true;
     }
 }
