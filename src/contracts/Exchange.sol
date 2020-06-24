6a0e9b12a6f81bc2ce2e556823b8fb88239f95ed
Anar Enhsaihan <aenhsaihan@gmail.com>
2020-06-24 16:55:45 -0400

    Allow user to deposit tokens into exchange

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.5.0;
 
+import "./Token.sol";
+
 contract Exchange {
     address public feeAccount;
     uint256 public feePercent;
@@ -7,5 +9,9 @@ contract Exchange {
     constructor(address _feeAccount, uint256 _feePercent) public {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
+    }
+
+    function depositToken(address _token, uint256 _amount) public {
+        require(Token(_token).transferFrom(msg.sender, address(this), _amount));
     }
 }
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -1,17 +1,20 @@
 import { tokens, EVM_REVERT } from "./helpers";
-import { iteratee } from "lodash";
 
 const Exchange = artifacts.require("./Exchange");
+const Token = artifacts.require("./Token");
 
 require("chai")
   .use(require("chai-as-promised"))
   .should();
 
-contract("Exchange", ([deployer, feeAccount]) => {
-  let exchange;
+contract("Exchange", ([deployer, feeAccount, user1]) => {
+  let exchange, token, amount;
   const feePercent = 10;
 
   beforeEach(async () => {
+    token = await Token.new();
+    token.transfer(user1, tokens(100, { from: deployer }));
+
     exchange = await Exchange.new(feeAccount, feePercent);
   });
 
@@ -24,6 +27,37 @@ contract("Exchange", ([deployer, feeAccount]) => {
     it("it tracks the fee percent", async () => {
       const result = await exchange.feePercent();
       result.toString().should.equal(feePercent.toString());
+    });
+  });
+
+  describe("depositing tokens", () => {
+    describe("success", () => {
+      let result, amount;
+
+      beforeEach(async () => {
+        amount = tokens(10);
+        result = await token.approve(exchange.address, amount, {
+          from: user1,
+        });
+
+        result = await exchange.depositToken(token.address, amount, {
+          from: user1,
+        });
+      });
+
+      it("tracks the token deposit", async () => {
+        let balance;
+        balance = await token.balanceOf(exchange.address);
+        balance.toString().should.equal(amount.toString());
+      });
+    });
+
+    describe("failure", () => {
+      it("rejects unapproved transfers", async () => {
+        await exchange.depositToken(token.address, amount, {
+          from: deployer,
+        }).should.be.rejected;
+      });
     });
   });
 });
