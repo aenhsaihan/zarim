04bd71ebfbb2586b6ff983a4ff62c5e8d9db4bb8
Anar Enhsaihan <aenhsaihan@gmail.com>
2020-06-24 10:54:52 -0400

    Establish a fee percentage on orders
    Each time a user places an order to buy or sell, a fee percentage will be applied.

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -2,8 +2,10 @@ pragma solidity ^0.5.0;
 
 contract Exchange {
     address public feeAccount;
+    uint256 public feePercent;
 
-    constructor(address _feeAccount) public {
+    constructor(address _feeAccount, uint256 _feePercent) public {
         feeAccount = _feeAccount;
+        feePercent = _feePercent;
     }
 }
